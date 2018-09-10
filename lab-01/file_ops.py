aee45d5235bc874b8cfc9dcd98b842e9f83d8c01
Chandan Kumar <chandank@becloudready.com>
2018-09-09 21:11:13 -0500

    added file read commands

--- lab-01/file_ops.py
+++ lab-01/file_ops.py
@@ -4,13 +4,15 @@
 # Open the license file, read the content into the buffer 
 # then close the file descriptor
 
-f = open('LICENSE')
-file_content = f.read()
+f = open('conditions.py')
+lines10 = f.read(10)
+#alllines = f.read()
+print (lines10)
 f.close()
 
 # Open the license file, read the content as "lines" into the 
 # buffer and close it.
 
-f = open('LICENSE')
+f = open('conditions.py')
 file_content = f.readlines()
 f.close()
