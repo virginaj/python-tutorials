36731190b38871be8686bc44c3554f2a486066dd
virginaj <42753466+virginaj@users.noreply.github.com>
2018-09-13 11:56:19 -0500

    Update ec-create-instance.py

--- lab-07/ec-create-instance.py
+++ lab-07/ec-create-instance.py
@@ -5,7 +5,7 @@ def create_instance():
     ec2_resource = boto3.resource('ec2')
     instances = ec2_resource.create_instances(ImageId='ami-6871a115',
                 MinCount=1, MaxCount=3,InstanceType='t2.micro',
-                SecurityGroupIds=['lauch-wizard-7'],KeyName='fullstack')
+                SecurityGroupIds=['launch-wizard-3'],KeyName='fullstack')
     instance_ids = []
     for instance in instances:
         instance_ids.append(instance.id)
